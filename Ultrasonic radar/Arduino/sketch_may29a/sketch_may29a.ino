c7ca6aa723b29865830c4847a3c32e1656fcecaa
MoPaZ <freddienordstedt@gmail.com>
2015-05-29 20:44:54 +0200

    Cleanup
    Before changing to Process processing

--- Ultrasonic radar/Arduino/sketch_may29a/sketch_may29a.ino
+++ Ultrasonic radar/Arduino/sketch_may29a/sketch_may29a.ino
@@ -1,36 +1,51 @@
 #include <NewPing.h>
 
+// Define sonar pins
 #define TRIGGER_PIN1 7
 #define ECHO_PIN1 6
 #define TRIGGER_PIN2 4
 #define ECHO_PIN2 3
-
 #define MAX_DISTANCE 200
 
+// Define led pins
+#define LedL2 12
+#define LedL1 11
+#define LedC 10
+#define LedR1 9
+#define LedR2 8
+
+// Init sonar objects
 NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
 NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+
+// Variableeessssss
+int diff;
+
 void setup() {
-  // put your setup code here, to run once:
+  pinMode(LedL2, OUTPUT);
+  pinMode(LedL1, OUTPUT);
+  pinMode(LedC, OUTPUT);
+  pinMode(LedR1, OUTPUT);
+  pinMode(LedR2, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  unsigned int uS1 = sonar1.ping();
-  delay(50);
-  unsigned int uS2 = sonar2.ping();
-//  Serial.print("Ping1: ");
-//  Serial.print(uS1 / US_ROUNDTRIP_CM);
-//  Serial.println("cm");
-  delay(50);
-  Serial.print(uS1 / US_ROUNDTRIP_CM);
+
+  // Ping thoose sonars!!
+  unsigned int uS1 = sonar1.ping() / US_ROUNDTRIP_CM;      // Ping sensor left
+  delay(50);                                               // Delay for interference
+  unsigned int uS2 = sonar2.ping() / US_ROUNDTRIP_CM;      // Ping sensor right
+  delay(50);                                               // Funny delay
+  
+  // Calculating stuff
+  diff = uS1-uS2;
+
+  // Serial stuff
+  Serial.print(uS1);
   Serial.print(" ");
-  Serial.println(uS2 / US_ROUNDTRIP_CM);
-  
-  
-  //unsigned int uS2 = sonar2.ping();
-//  Serial.print("Ping2: ");
-//  Serial.print(uS2 / US_ROUNDTRIP_CM);
-//  Serial.println("cm");
-  
-  }
+  Serial.print(uS2);
+  Serial.print(" ");
+  Serial.println(diff);
+
+}
